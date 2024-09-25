955b3d81e839cbb642734d7a61edd3e4f17109af
loiccltz <loic.collatuzo@gmail.com>
2024-09-25 18:50:37 +0200

    arg coord

--- src/engine/run.go
+++ src/engine/run.go
@@ -11,7 +11,7 @@ func (engine *Engine) Run() {
 	// si l'option -f est utilisée, affiche les FPS
 	showFPS := flag.Bool("f", false, "Affiche les FPS")
 	fullscreen := flag.Bool("fullscreen", false, "Lance le jeu en plein ecran")
-	
+	showCoord := flag.Bool("coord", false, "Affiche les coordonnées du joueur")
 	flag.Parse()
 
         // Si l'option -f est utilisée, afficher les FPS
@@ -61,6 +61,11 @@ func (engine *Engine) Run() {
             rl.DrawText(fmt.Sprintf("FPS: %d", fps), 10, 10, 20, rl.DarkGray)
         }
 
+		if *showCoord {
+			// obliger de mettre 1 apres la virgule les coord sont en float
+			coord := fmt.Sprintf("X: %.1f, Y: %.1f", engine.Player.Position.X, engine.Player.Position.Y)
+			rl.DrawText(coord, 20, 80, 20, rl.DarkGray)
+		}
 		if *fullscreen {
 			rl.ToggleFullscreen()
 		}
